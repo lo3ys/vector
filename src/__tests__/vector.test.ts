9ca7d18a03fa972da898726eb4250d0798ce8a97
netscape-swega <34111613+netscape-swega@users.noreply.github.com>
2022-09-01 17:05:27 +0200

    first commit

---
+++ src/__tests__/vector.test.ts
@@ -0,0 +1,214 @@
+import { Vector } from "../vector";
+
+describe("add with 2 and 3 dimensions", () => {
+
+    test("add value 2D", () => {
+        const vector = new Vector(5, 8);
+        vector.add(5);
+        const vectorResult = new Vector(10, 13);
+        Object.is(vector, vectorResult);
+    });
+
+    test("add value 3D", () => {
+        const vector = new Vector(5, 8, 10);
+        vector.add(5);
+        const vectorResult = new Vector(10, 13, 15);
+        Object.is(vector, vectorResult);
+    });
+
+    test("add vector", () => {
+        const vector = new Vector(1, 2);
+        const vector2 = new Vector(3, 4);
+        vector.add(vector2);
+        const vectorResult = new Vector(4, 6);
+        Object.is(vector, vectorResult);
+    });
+
+    test("add vector 3D", () => {
+        const vector = new Vector(1, 2, 5);
+        const vector2 = new Vector(3, 4, 2);
+        vector.add(vector2);
+        const vectorResult = new Vector(4, 6, 7);
+        Object.is(vector, vectorResult);
+    });
+});
+
+describe("subtract with 2 and 3 dimensions", () => {
+
+    test("sub value", () => {
+        const vector = new Vector(5, 8);
+        vector.sub(5);
+        const vectorResult = new Vector(0, 3);
+        Object.is(vector, vectorResult);
+    });
+
+    test("sub value 3D", () => {
+        const vector = new Vector(5, 8, 15);
+        vector.sub(5);
+        const vectorResult = new Vector(0, 3, 10);
+        Object.is(vector, vectorResult);
+    });
+    
+    test("sub vector", () => {
+        const vector = new Vector(1, 2);
+        const vector2 = new Vector(3, 4);
+        vector.sub(vector2);
+        const vectorResult = new Vector(-2, -2);
+        Object.is(vector, vectorResult);
+    });
+
+    test("sub vector 3D", () => {
+        const vector = new Vector(1, 2, 8);
+        const vector2 = new Vector(3, 4, 0.5);
+        vector.sub(vector2);
+        const vectorResult = new Vector(-2, -2, 7.5);
+        Object.is(vector, vectorResult);
+    });
+});
+
+
+describe("multiply with 2 and 3 dimensions", () => {
+
+    test("mult value", () => {
+        const vector = new Vector(5, 8);
+        vector.mult(5);
+        const vectorResult = new Vector(25, 40);
+        Object.is(vector, vectorResult);
+    });
+
+    test("mult value 3D", () => {
+        const vector = new Vector(5, 8, 15);
+        vector.mult(5);
+        const vectorResult = new Vector(25, 40, 75);
+        Object.is(vector, vectorResult);
+    });
+    
+    test("mult vector", () => {
+        const vector = new Vector(1, 2);
+        const vector2 = new Vector(3, 4);
+        vector.mult(vector2);
+        const vectorResult = new Vector(3, 8);
+        Object.is(vector, vectorResult);
+    });
+
+    test("mult vector 3D", () => {
+        const vector = new Vector(1, 2, 5);
+        const vector2 = new Vector(3, 4, 6);
+        vector.mult(vector2);
+        const vectorResult = new Vector(3, 8, 30);
+        Object.is(vector, vectorResult);
+    });
+})
+
+describe("divide with 2 and 3 dimensions", () => {
+
+    test("div value", () => {
+        const vector = new Vector(5, 8);
+        vector.div(5);
+        const vectorResult = new Vector(1, 1.6);
+        Object.is(vector, vectorResult);
+    });
+
+    test("div value 3D", () => {
+        const vector = new Vector(5, 8, 15);
+        vector.div(5);
+        const vectorResult = new Vector(1, 1.6, 3);
+        Object.is(vector, vectorResult);
+    });
+    
+    test("div vector", () => {
+        const vector = new Vector(1, 2);
+        const vector2 = new Vector(3, 4);
+        vector.div(vector2);
+        const vectorResult = new Vector(1/3, 0.5);
+        Object.is(vector, vectorResult);
+    });
+
+    test("div vector 3D", () => {
+        const vector = new Vector(1, 2, 8);
+        const vector2 = new Vector(3, 4, 64);
+        vector.div(vector2);
+        const vectorResult = new Vector(1/3, 0.5, 6);
+        Object.is(vector, vectorResult);
+    });
+
+});
+
+describe("randomizing 2D vectors", () => {
+    test("random 2D", () => {
+        let vArr: Vector[] = [];
+        const length = 20;
+        let occurences = 0;
+
+        for (let i = 0; i < length; i++)
+            vArr.push(Vector.random2D(0, 20));
+
+        for (const vec of vArr) {
+            const count = vArr.filter(v => v == vec).length;
+            occurences += count;
+        }
+        
+        expect(vArr.length).toBe(length);
+        expect(occurences).toBe(vArr.length);
+    });
+
+    test("random Gaussian 2D", () => {
+        let vArr: Vector[] = [];
+        const length = 20;
+        let occurences = 0;
+
+        for (let i = 0; i < length; i++)
+            vArr.push(Vector.randomGaussian2D(0, 20));
+
+        for (const vec of vArr) {
+            const count = vArr.filter(v => v == vec).length;
+            occurences += count;
+        }
+        
+        expect(vArr.length).toBe(length);
+        expect(occurences).toBe(vArr.length);
+    });
+});
+
+describe("randomizing 3D vectors", () => {
+    test("random 3D", () => {
+        let vArr: Vector[] = [];
+        const length = 20;
+        let occurences = 0;
+
+        for (let i = 0; i < length; i++)
+            vArr.push(Vector.random3D(0, 20));
+
+        for (const vec of vArr) {
+            const count = vArr.filter(v => v == vec).length;
+            occurences += count;
+        }
+        
+        expect(vArr.length).toBe(length);
+        expect(occurences).toBe(vArr.length);
+    });
+
+    test("random Gaussian 3D", () => {
+        let vArr: Vector[] = [];
+        const length = 20;
+        let occurences = 0;
+
+        for (let i = 0; i < length; i++)
+            vArr.push(Vector.randomGaussian3D(0, 20));
+
+        for (const vec of vArr) {
+            const count = vArr.filter(v => v == vec).length;
+            occurences += count;
+        }
+        
+        expect(vArr.length).toBe(length);
+        expect(occurences).toBe(vArr.length);
+    });
+});
+
+
+
+
+
+
+
---
+++ src/vector.ts
@@ -0,0 +1,162 @@
+
+class Vector {
+
+    x: number;
+    y: number;
+    z: number | null;
+
+    constructor(x: number, y: number, z?: number){
+        this.x = x;
+        this.y = y;
+        this.z = (z)?(z):(null);
+    }
+
+    public add(value: number): Vector;
+    public add(vector: Vector): Vector;
+    public add(value: any): Vector{
+        if (typeof value == "number")
+        {
+            this.x += value;
+            this.y += value;
+            if (this.z) this.z += value;
+            return this;
+        }
+        else if (value instanceof Vector)
+        {
+            this.x += value.x;
+            this.y += value.y;
+            if (this.z && value.z) this.z += value.z;
+            return this;
+        }
+        throw new Error("Types not matching")
+    }
+
+    public sub(value: number): Vector;
+    public sub(vector: Vector): Vector;
+    public sub(value: any): Vector{
+        if (typeof value == "number")
+        {
+            this.x -= value;
+            this.y -= value;
+            if (this.z) this.z -= value;
+            return this;
+        }
+        else if (value instanceof Vector)
+        {
+            this.x -= value.x;
+            this.y -= value.y;
+            if (this.z && value.z) this.z -= value.z;
+            return this;
+        }
+        throw new Error("Types not matching")
+    }
+
+    public mult(value: number): Vector;
+    public mult(vector: Vector): Vector;
+    public mult(value: any): Vector{
+        if (typeof value == "number")
+        {
+            this.x *= value;
+            this.y *= value;
+            if (this.z) this.z *= value;
+            return this;
+        }
+        else if (value instanceof Vector)
+        {
+            this.x *= value.x;
+            this.y *= value.y;
+            if (this.z && value.z) this.z *= value.z;
+            return this;
+        }
+        throw new Error("Types not matching")
+    }
+
+    public div(value: number): Vector;
+    public div(vector: Vector): Vector;
+    public div(value: any): Vector{
+        if (typeof value == "number")
+        {
+            this.x /= value;
+            this.y /= value;
+            if (this.z) this.z /= value;
+            return this;
+        }
+        else if (value instanceof Vector)
+        {
+            this.x /= value.x;
+            this.y /= value.y;
+            if (this.z && value.z) this.z /= value.z;
+            return this;
+        }
+        throw new Error("Types not matching")
+    }
+
+    static random2D(min: number, max: number): Vector{
+        const vec = new Vector(0, 0);
+        vec.x = Vector.getRandomInRange(min, max);
+        vec.y = Vector.getRandomInRange(min, max);
+        return vec;
+    }
+
+    static random3D(min: number, max: number): Vector{
+        const vec = new Vector(0, 0, 0);
+        vec.x = Vector.getRandomInRange(min, max);
+        vec.y = Vector.getRandomInRange(min, max);
+        vec.z = Vector.getRandomInRange(min, max);
+        return vec;
+
+    }
+
+    static randomGaussian2D(min: number, max: number, skew: number = 1): Vector{
+        const vec = new Vector(0, 0);
+        vec.x = Vector.randn_bm(min, max, skew);
+        vec.y = Vector.randn_bm(min, max, skew);
+        return vec;
+    }
+
+    static randomGaussian3D(min: number, max: number, skew: number = 1): Vector{
+        const vec = new Vector(0, 0, 0);
+        vec.x = Vector.randn_bm(min, max, skew);
+        vec.y = Vector.randn_bm(min, max, skew);
+        vec.z = Vector.randn_bm(min, max, skew);
+        return vec;
+    }
+
+    /**
+     * random floating number within a range
+     * @param min 
+     * @param max 
+     * @returns 
+     */
+    static getRandomInRange(min: number, max: number) {
+        return Math.random() * (max - min) + min;
+    }
+
+    /**
+     * normal distribution random floating number withing a range
+     * @param min fisrt minimum of the curve (base of teh range)
+     * @param max last minimum of the curve (top of the range)
+     * @param skew displacement of the curve (displacement in the distribution of the numbers)
+     * @returns 
+     */
+    static randn_bm(min: number, max: number, skew: number = 1) {
+        let u = 0, v = 0;
+        while(u === 0) u = Math.random() //Converting [0,1) to (0,1)
+        while(v === 0) v = Math.random()
+        let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )
+        
+        num = num / 10.0 + 0.5 // Translate to 0 -> 1
+        if (num > 1 || num < 0) 
+          num = this.randn_bm(min, max, skew) // resample between 0 and 1 if out of range
+        
+        else{
+          num = Math.pow(num, skew) // Skew
+          num *= max - min // Stretch to fill range
+          num += min // offset to min
+        }
+        return num
+    }
+}
+
+
+export { Vector };
